7989116462efabc2da1d32e65b02893189a4275e
iamtun <letuan19431791.iuh@gmail.com>
2024-02-25 09:29:44 +0700

    create server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-﻿console.log(process.env.SECRET_KEY);
+﻿import express from 'express';
+
+//constant
+const PORT = process.env.PORT;
+
+
+const app = express();
+
+
+//register middlewares
+app.use(express.json({extended: false}));
+app.use(express.static('./views'));
+
+//config view
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
